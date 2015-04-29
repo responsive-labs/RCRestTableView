f28f51d657b36c5d9f6ded3d4eff93fe440b95a0
Luca Serpico <serpicoluca@gmail.com>
2015-04-29 09:32:50 +0100

    Added structure tests

--- Example/Tests/RCRestTableStructureTests.m
+++ Example/Tests/RCRestTableStructureTests.m
@@ -6,7 +6,9 @@
 //  Copyright (c) 2015 Luca Serpico. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import "RCRestTableStructure.h"
+#import "RCRestTableViewKeys.h"
 #import <Specta/Specta.h>
 #import <Expecta/Expecta.h>
 
@@ -28,6 +30,19 @@ describe(@"Structure", ^{
 	it(@"rows in section", ^{
 		expect([[structure rowsInSection:0] count]).to.equal(3);
 		expect([[structure rowsInSection:1] count]).to.equal(1);
+	});
+	
+	it(@"section at index", ^{
+		NSDictionary *fromSections = [[structure sections] objectAtIndex:0];
+		NSDictionary *fromStructure = [structure sectionAtIndex:0];
+		expect(fromSections).to.equal(fromStructure);
+	});
+	
+	it(@"row at indexPath", ^{
+		NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+		NSDictionary *fromSections = [[[structure sectionAtIndex:indexPath.section] objectForKey:kRCRestKeyRows] objectAtIndex:indexPath.row];
+		NSDictionary *fromStructure = [structure rowAtIndexPath:indexPath];
+		expect(fromSections).to.equal(fromStructure);
 	});
 });
 
